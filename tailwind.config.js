94a75ed7d567aaf4e2c954260495ef658b7b0271
AnaGCastro <testeanateste0@gmail.com>
2025-05-20 13:07:13 -0300

    Finalizando Hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}", // ou adapte para seu projeto
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'uber': "url('./src/assets/bguber.png')",
+        'city': "url('./src/assets/bg-city.png')"
+      },
+    },
   },
   plugins: [],
 }
-
