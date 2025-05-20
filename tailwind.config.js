bafad96f8e2d6d9e311b78ce5e158c40a7d2b897
AnaGCastro <testeanateste0@gmail.com>
2025-05-20 10:23:22 -0300

    Criando estrturua do projeto Uber

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // ou adapte para seu projeto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
